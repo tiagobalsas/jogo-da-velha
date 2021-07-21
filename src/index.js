2c32cac3fb801094b30319171990cf8997e8bfce
Tiago Santos <tiago881@gmail.com>
2021-07-21 10:31:25 -0300

    Add props no elemento botão

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className='square' onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
-      </button>
+      <button className='square' onClick={() => this.props.onClick()}>
+      {this.props.value}
+    </button>
     );
   }
 }
