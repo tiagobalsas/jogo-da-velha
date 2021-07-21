93c3a131b2b1ab455ff25050a4d29345bdd6c74d
Tiago Santos <tiago881@gmail.com>
2021-07-21 10:29:34 -0300

    Deleta o construtor do componente Square

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
       <button className='square' onClick={() => this.setState({ value: 'X' })}>
